ceb254d127fe7d11690f2adf02ddbb853e137434
Francois Dion <francois.dion@gmail.com>
2014-10-22 21:55:53 -0400

    Update test_toxsample.py

--- tox/tests/test_toxsample.py
+++ tox/tests/test_toxsample.py
@@ -8,5 +8,9 @@ class TestToxSample(unittest.TestCase):
     def test_is_shiningpanda_amazing(self):
         self.assertTrue(toxsample.is_shiningpanda_amazing())
 
+    def test_fail_on_3(self):
+        print "Not gonna happen"
+        self.assertTrue(True)
+
 if __name__ == '__main__':
     unittest.main()
